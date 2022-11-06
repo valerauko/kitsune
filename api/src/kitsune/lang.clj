9a2dac8799a34ddca6ddf44d7ca97392a8c6ee39
Vale <valerauko@gmail.com>
2022-11-07 00:06:20 +0900

    Add macro helper

--- api/src/kitsune/lang.clj
+++ api/src/kitsune/lang.clj
@@ -10,3 +10,14 @@
         (if (symbol? head)
           (recur (assoc aggr (keyword head) head) tail)
           (recur (assoc aggr head (first tail)) (rest tail)))))))
+
+(defn qualify-sym
+  "From https://stackoverflow.com/a/71585112. No idea why there is no built-in."
+  [s]
+  (if (simple-symbol? s)
+    (or (some-> (ns-refers *ns*) (get s) symbol)
+        (symbol (str *ns*) (str s)))
+    (let [nsp (namespace s)
+          n (name s)
+          aliases (ns-aliases *ns*)]
+      (symbol (or (some-> aliases (get (symbol nsp)) ns-name str) nsp) n))))
