3837a0941b3ba665c56b8e31ca9872e01fb9a5d1
Vale <valerauko@gmail.com>
2022-11-07 00:11:04 +0900

    Server setup

---
+++ api/src/kitsune/core.clj
@@ -0,0 +1,63 @@
+(ns kitsune.core
+  (:require [clojure.tools.logging :as log]
+            [mount.core :refer [defstate start stop]]
+            [kitsune.db]
+            [kitsune.db.migration :refer [migrate rollback]]
+            [org.httpkit.server :as http]
+            [kitsune.routes :as routes]))
+
+(set! *warn-on-reflection* true)
+(set! *unchecked-math* :warn-on-boxed)
+
+(defn wrap-async
+  [ring-handler]
+  (fn -async-wrap [request]
+    (when-let [channel (:async-channel request)]
+      (try
+        (let [response (ring-handler request)]
+          (http/send! channel response))
+        (catch Throwable error
+          (log/error "Error while processing request" error)
+          (http/send! channel {:status 500})
+          (http/close channel)))
+      {:body channel})))
+
+(defstate http-server
+  :start
+  (let [server (http/run-server
+                (wrap-async routes/handler)
+                {:port 3000
+                 :max-body 44739243})]
+    (log/info "API server running at :3000")
+    server)
+  :stop
+  (do
+    (log/info "Stopping server...")
+    (http-server :timeout 1000)))
+
+(defn shutdown
+  [& _]
+  (log/info "Shutting down...")
+  (stop))
+
+(defn -main
+  [& args]
+  (cond
+    (some #{"migrate"} args)
+    (do
+      (start #'kitsune.db/datasource)
+      (migrate)
+      (System/exit 0))
+
+    (some #{"rollback"} args)
+    (do
+      (start #'kitsune.db/datasource)
+      (rollback)
+      (System/exit 0))
+
+    :else
+    (do
+      (start)
+      (.addShutdownHook
+       (Runtime/getRuntime)
+       (Thread. ^Runnable shutdown)))))
---
+++ api/src/kitsune/routes.clj
@@ -0,0 +1,58 @@
+(ns kitsune.routes
+  (:require [muuntaja.core :as muuntaja]
+            [reitit.coercion.malli :as coercion]
+            [reitit.ring]
+            [reitit.ring.coercion :refer [coerce-exceptions-middleware
+                                          coerce-request-middleware
+                                          coerce-response-middleware]]
+            [reitit.ring.middleware.parameters :refer [parameters-middleware]]
+            [reitit.ring.middleware.muuntaja :refer [format-middleware]]
+            [reitit.swagger :refer [create-swagger-handler]]
+            [ring.middleware.keyword-params :refer [wrap-keyword-params]]
+            [kitsune.fed.routes :as fed]
+            [kitsune.uri :as uri]
+            [kitsune.well-known.routes :as well-known]
+            [kitsune.wrappers.format :as fmt :refer [wrap-format]]
+            [kitsune.wrappers.logging :refer [wrap-logging]])
+  (:import [clojure.lang
+            Keyword]))
+
+(def router
+  (reitit.ring/router
+   [["/@:name"
+     {:name ::profile
+      :parameters {:path {:name :string}}}]
+    fed/routes
+    well-known/routes
+    ["/openapi.json"
+     {:get {:no-doc true
+            :swagger {:info {:title "Kitsune API"
+                             :description "Very fox microblogging"
+                             :version "0.3.0"}
+                      :basePath "/"}
+            :handler (create-swagger-handler)}}]]
+   {:data
+    {:muuntaja (muuntaja/create fmt/default-options)
+     :coercion (coercion/create {})
+     :middleware
+     [parameters-middleware
+      format-middleware
+      wrap-format
+      coerce-exceptions-middleware
+      coerce-response-middleware
+      coerce-request-middleware]}}))
+
+;; close over router to avoid circular dependency in handlers
+(extend-protocol uri/Url
+  Keyword
+  (uri/url
+   ([kw] (uri/url kw {}))
+   ([kw args]
+    (uri/url-with-router router kw args))))
+
+(def handler
+  (-> router
+      (reitit.ring/ring-handler
+       (reitit.ring/create-default-handler)
+       {:inject-router false})
+      wrap-logging))
