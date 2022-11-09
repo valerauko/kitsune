5ae2796f4b6688763d196d200d46fa65ceeaa504
Vale <valerauko@gmail.com>
2022-11-09 19:51:33 +0900

    Fix upsert (it shouldn't change primary key)

--- api/src/kitsune/db/account.clj
+++ api/src/kitsune/db/account.clj
@@ -1,4 +1,5 @@
 (ns kitsune.db.account
+  (:refer-clojure :exclude [update])
   (:require [next.jdbc :refer [with-transaction]]
             [kitsune.db :as db :refer [defquery]]))
 
@@ -40,8 +41,33 @@
    :where [:= :account-id account-id]
    :limit 1})
 
-(defquery upsert
+(defquery select-for-update
+  [uri]
+  {:select [:*]
+   :for :update
+   :from [:accounts]
+   :where [:= :uri (str uri)]
+   :limit 1})
+
+(defquery update
+  [{:keys [uri] :as account}]
+  (let [update-keys (keys (dissoc account :id :uri :created-at :updated-at))]
+    {:update [:accounts]
+     :set (select-keys account update-keys)
+     :where [:= :uri uri]
+     :limit 1}))
+
+(defquery create
   [account]
-  {:replace-into :accounts
+  {:insert-into [:accounts]
    :values [account]
    :returning [:*]})
+
+(defn upsert
+  [{:keys [uri] :as account}]
+  (with-transaction [tx db/datasource]
+    (if-let [local (select-for-update uri)]
+      (do
+        (update account)
+        (find-by-uri uri))
+      (create account))))
