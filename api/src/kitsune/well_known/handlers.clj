80dc2129021568096a55e444f03c8ddd98fe93fc
Vale <valerauko@gmail.com>
2022-11-14 16:14:42 +0900

    Allow webfingering accounts by uri not just acct

--- api/src/kitsune/well_known/handlers.clj
+++ api/src/kitsune/well_known/handlers.clj
@@ -2,9 +2,12 @@
   (:require [ring.util.http-response :refer [ok not-found]]
             [jsonista.core :as json]
             [next.jdbc :as jdbc]
+            [kitsune.lang :refer [cond-let]]
             [kitsune.uri :as uri :refer [url]]
             [kitsune.db.account :refer [find-local-by-name]]
-            [kitsune.fed.routes :as-alias fed]))
+            [kitsune.fed.routes :as-alias fed])
+  (:import [java.net
+            URI]))
 
 (defn host-meta
   [{method :method :as req}]
@@ -46,36 +49,55 @@
             {:rel "http://nodeinfo.diaspora.software/ns/schema/2.1"
              :href "https://kitsune.loca.lt/.well-known/nodeinfo/2.1"}]}))
 
+(defn local-uri->account-name
+  [maybe-uri]
+  (when-let [uri (try
+                   (URI. maybe-uri)
+                   (catch Throwable _
+                     nil))]
+    (when (= (.getHost uri) uri/host)
+      (clojure.tools.logging/debug "ok host")
+      (when-let [match (uri/match (.getPath uri))]
+        (clojure.tools.logging/debug "ok match")
+        (get-in match [:path-params :name])))))
+
+(defn resource->account
+  [resource]
+  (cond-let
+   [user (local-uri->account-name resource)]
+   (find-local-by-name user)
+
+   [[user host] (->> resource
+                     (re-find #"(?i)^(?:acct:)?@?([^@]+)(?:@([^\pZ]+))?")
+                     (rest)
+                     (not-empty))]
+   (when (or (empty? host) (= host uri/host))
+     (find-local-by-name user))))
+
 (defn webfinger
   [{fmt :muuntaja/request
     {{:keys [resource]} :query} :parameters}]
-  (if-let [[user host] (->> resource
-                            (re-find #"(?i)(?:acct:)?@?([^@]+)(?:@([^\pZ]+))?")
-                            (rest)
-                            (not-empty))]
-    (if (or (empty? host) (= host uri/host))
-      (if-let [{:accounts/keys [acct name uri]} (find-local-by-name user)]
-        (let [subject (str "acct:" acct)
-              links [{:href (url :kitsune.routes/profile {:name name})
-                      :rel "http://webfinger.net/rel/profile-page"
-                      :type "text/html"}
-                     {:href uri
-                      :rel "self"
-                      :type "application/activity+json"}
-                     {:rel "http://ostatus.org/schema/1.0/subscribe"
-                      ;; reitit's match-to-path in `url` escapes the {} so str
-                      :template (str (url ::fed/authorize-follow) "?acct={uri}")}]]
-          (ok
-           (case fmt
-             ("application/xml" "application/xrd+xml")
-             (reduce
-              conj
-              [:XRD
-               {:xmlns "http://docs.oasis-open.org/ns/xri/xrd-1.0"}
-               [:Subject subject]]
-              (map (fn [attrs] [:Link attrs]) links))
-             ;; else
-             {:subject subject
-              :links links})))
-        (not-found))
-      (not-found))))
+  (if-let [{:accounts/keys [acct name uri]} (resource->account resource)]
+    (let [subject (str "acct:" acct)
+          links [{:href (url :kitsune.routes/profile {:name name})
+                  :rel "http://webfinger.net/rel/profile-page"
+                  :type "text/html"}
+                 {:href uri
+                  :rel "self"
+                  :type "application/activity+json"}
+                 {:rel "http://ostatus.org/schema/1.0/subscribe"
+                  ;; reitit's match-to-path in `url` escapes the {} so str
+                  :template (str (url ::fed/authorize-follow) "?acct={uri}")}]]
+      (ok
+       (case fmt
+         ("application/xml" "application/xrd+xml")
+         (reduce
+          conj
+          [:XRD
+           {:xmlns "http://docs.oasis-open.org/ns/xri/xrd-1.0"}
+           [:Subject subject]]
+          (map (fn [attrs] [:Link attrs]) links))
+         ;; else
+         {:subject subject
+          :links links})))
+    (not-found)))
