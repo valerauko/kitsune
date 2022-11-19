0287eb66eafcf8a4fd1059b7d68653ae3002d129
Vale <valerauko@gmail.com>
2022-11-20 02:13:20 +0900

    Don't create follow when one already exists

--- api/src/kitsune/fed/inbox/follow.clj
+++ api/src/kitsune/fed/inbox/follow.clj
@@ -1,7 +1,7 @@
 (ns kitsune.fed.inbox.follow
   (:require [kitsune.async :refer [delayed]]
             [kitsune.db.account :refer [find-local-by-uri]]
-            [kitsune.db.follow :refer [create-follow]]
+            [kitsune.db.follow :refer [create-follow find-follow]]
             [kitsune.fed.outbox.accept :refer [accept-follow]]
             [kitsune.lang :refer [...]]))
 
@@ -9,7 +9,8 @@
   [{:keys [id object]
     {follower-id :accounts/id :as remote-account} :remote-account}]
   (when-let [{followed-id :accounts/id} (find-local-by-uri object)]
-    (create-follow (... follower-id followed-id))
+    (when-not (find-follow (... follower-id followed-id))
+      (create-follow (... follower-id followed-id)))
     ;; TODO: only send accept if local auto-accepts follows
     (delayed (accept-follow (... id object
                                  :actor (:accounts/uri remote-account))))))
