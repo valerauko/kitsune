68149ecfb178de29c5450a02fc18d22a288c06bb
Vale <valerauko@gmail.com>
2022-11-20 22:31:13 +0900

    Rework http event api

--- web/src/kitsune/http/events.cljs
+++ web/src/kitsune/http/events.cljs
@@ -1,63 +1,48 @@
 (ns kitsune.http.events
-  (:require [cljs.reader :as reader]
-            [re-frame.core :as rf]
-            [kitsune.effects :as fx]))
+  (:require [re-frame.core :as rf]
+            [kitsune.http.effects :as fx]))
 
 (rf/reg-event-fx
  ::load
  (fn [{{{:keys [in-flight]} ::state :as db} :db :or {in-flight 0}}
-      [_ {:keys [path coords loaded opts]}]]
+      [_ {:keys [coords on-final on-success on-error opts path]}]]
    (let [state (get-in db (cons ::state coords) {:state :new})]
+     ;; don't fire more than one request for the same coords
      (when-not (= (:state state) :loading)
        (let [callback {:coords coords
-                       :loaded loaded}]
+                       :loaded on-final
+                       :on-success on-success
+                       :on-error on-error}]
          {:db (assoc-in db [::state :in-flight] (inc in-flight))
           ::fx/http {:path path
                      :opts opts
                      :on-success [::http-success callback]
                      :on-error [::http-error callback]}})))))
 
-(defn timestamps
-  [{:keys [created-at updated-at] :as obj}]
-  (-> obj
-      (assoc :created-at (reader/parse-timestamp created-at))
-      (assoc :updated-at (reader/parse-timestamp updated-at))))
-
-(defn convert
-  [body]
-  (let [parsed (js->clj body :keywordize-keys true)]
-    ; (cond
-    ;   (vector? parsed) (mapv timestamps parsed)
-    ;   (map? parsed) (timestamps parsed)
-    ;   :else parsed))
-    parsed))
-
 (rf/reg-event-fx
  ::http-success
- (fn [{db :db} [_ {:keys [coords] :as opts} body]]
-   {:db (-> db
-            (assoc-in (cons ::state coords)
-                      {:state :ready})
-            (assoc-in (cons :data coords)
-                      (convert body)))
-    :dispatch [::http-finish opts]}))
+ (fn [{db :db} [_ {:keys [on-success coords] :as opts} response]]
+   {:db (assoc-in db (cons ::state coords) {:state :ready})
+    :dispatch-n (let [dispatch [[::http-finish opts]]]
+                  (if on-success
+                    (conj dispatch [on-success response])
+                    dispatch))}))
 
 (rf/reg-event-fx
  ::http-error
- (fn [{db :db} [_ {:keys [coords] :as opts} _response]]
-   {:db (-> db
-            (assoc-in (cons ::state coords)
-                      {:state :error})
-            (assoc-in (cons :data coords)
-                      nil))
-    :dispatch [::http-finish opts]}))
+ (fn [{db :db} [_ {:keys [on-error coords] :as opts} response]]
+   {:db (assoc-in db (cons ::state coords) {:state :error})
+    :dispatch-n (let [dispatch [[::http-finish opts]]]
+                  (if on-error
+                    (conj dispatch [on-error response])
+                    dispatch))}))
 
 (rf/reg-event-fx
  ::http-finish
  (fn [{{{:keys [in-flight]} ::state :as db} :db :or {in-flight 1}}
-      [_ {:keys [coords loaded]}]]
+      [_ {:keys [coords on-final]}]]
    (let [new-in-flight (dec in-flight)
          result {:db (assoc-in db [::state :in-flight] new-in-flight)}]
-     (if (and loaded (< new-in-flight 1))
-       (assoc result :dispatch [loaded coords])
+     (if (and on-final (< new-in-flight 1))
+       (assoc result :dispatch [on-final coords])
        result))))
